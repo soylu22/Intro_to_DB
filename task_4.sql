ca397e2d0827eab1e1868e552dafe6ce76d86007
Leul Misganaw <leulmisganaw222@gmail.com>
2025-10-19 16:31:13 -0400

    add task_4.sql file

---
+++ task_4.sql
@@ -0,0 +1,10 @@
+-- Print the full description of the table 'Books' in alx_book_store
+SELECT COLUMN_NAME AS 'Column Name',
+       COLUMN_TYPE AS 'Data Type',
+       IS_NULLABLE AS 'Nullable',
+       COLUMN_KEY AS 'Key',
+       COLUMN_DEFAULT AS 'Default',
+       EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
