45459da2c41072c840b33fac5dc0c711745f52c9
Leul Misganaw <leulmisganaw222@gmail.com>
2025-10-19 16:32:52 -0400

    add task_5.sql file

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+
+-- Insert a single row into customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
